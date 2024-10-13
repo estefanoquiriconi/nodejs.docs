11ebf841bfd063c198289f41073c6d7de3646bd3
estefanoquiriconi <estefanoquiriconi@gmail.com>
2024-10-13 17:57:08 -0300

    Read and hash a file

---
+++ crypto.mjs
@@ -0,0 +1,13 @@
+import { createHash } from 'node:crypto';
+import { readFile } from 'node:fs/promises';
+
+const hasher = createHash('sha1');
+
+hasher.setEncoding('hex');
+
+hasher.write(await readFile('./package.json'));
+hasher.end();
+
+const fileHash = hasher.read();
+
+console.log(fileHash);
--- server.mjs
+++ server.mjs
@@ -1,11 +1,14 @@
 import { createServer } from 'node:http';
 
+const hostname = 'localhost';
+const port = 3000;
+
 const server = createServer((req, res) => {
     res.writeHead(200, { 'Content-Type' : 'text/plain'});
     res.end('Hello world!\n')
 });
 
-server.listen(3000, '127.0.0.1', () => {
-    console.log('Listening on 127.0.0.1:3000');
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
     
 })
